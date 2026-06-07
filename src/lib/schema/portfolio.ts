d07c4e76f8b72c68ced4b9f88b199fda5afa2f3f
ridleylab <labridley@gmail.com>
2026-06-07 17:25:44 +0700

    porto schema ganti itemlist

--- src/lib/schema/portfolio.ts
+++ src/lib/schema/portfolio.ts
@@ -1,3 +1,5 @@
+import { portfolioData } from "@/data/portfolio";
+
 export function createPortfolioSchema() {
   return {
     "@context": "https://schema.org",
@@ -10,5 +12,26 @@ export function createPortfolioSchema() {
       "Portofolio produksi kaos custom dan polo shirt untuk perusahaan, event, komunitas, dan instansi.",
 
     url: "https://www.garmento.id/portofolio",
+
+    itemListElement: portfolioData.map((item, index) => ({
+      "@type": "ListItem",
+
+      position: index + 1,
+
+      item: {
+        "@type": "CreativeWork",
+
+        name: item.title,
+
+        image: `https://www.garmento.id${item.image}`,
+
+        description: `${item.client} - ${item.qty} - ${item.teknik}`,
+
+        creator: {
+          "@type": "Organization",
+          name: "GARMENTO",
+        },
+      },
+    })),
   };
 }
