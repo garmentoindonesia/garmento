f793219eb5d9bd2e0635bad21c422aec98772caa
ridleylab <labridley@gmail.com>
2026-06-08 15:08:53 +0700

    login test

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,20 +3,22 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   const body = await req.json();
 
-  console.log("BODY:", body);
-
-  console.log(
-    "ENV:",
-    process.env.ADMIN_USERNAME,
-    process.env.ADMIN_PASSWORD
-  );
-
   const username = process.env.ADMIN_USERNAME;
   const password = process.env.ADMIN_PASSWORD;
 
   console.log("BODY:", body);
   console.log("USERNAME ENV:", username);
   console.log("PASSWORD ENV:", password);
+
+  console.log(
+    "USERNAME MATCH:",
+    body.username === username
+  );
+
+  console.log(
+    "PASSWORD MATCH:",
+    body.password === password
+  );
 
   if (
     body.username === username &&
@@ -26,13 +28,19 @@ export async function POST(req: Request) {
       success: true,
     });
 
-    response.cookies.set("admin_auth", "true", {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      path: "/",
-      maxAge: 60 * 60 * 24 * 7,
-    });
+    response.cookies.set(
+      "admin_auth",
+      "true",
+      {
+        httpOnly: true,
+        secure:
+          process.env.NODE_ENV ===
+          "production",
+        sameSite: "lax",
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7,
+      }
+    );
 
     return response;
   }
@@ -40,7 +48,8 @@ export async function POST(req: Request) {
   return NextResponse.json(
     {
       success: false,
-      message: "Username atau password salah",
+      message:
+        "Username atau password salah",
     },
     { status: 401 }
   );
