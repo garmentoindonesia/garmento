92a9a267c6bff403a518760be71c0da04e2e66f6
ridleylab <labridley@gmail.com>
2026-07-03 14:43:19 +0700

    hero beranda baru

--- src/components/sections/Beranda/Hero.tsx
+++ src/components/sections/Beranda/Hero.tsx
@@ -282,8 +282,8 @@ export default function Hero() {
                 hidden md:block
 
                 absolute
-                top-0
-                right-0
+                bottom-0
+                left-0
                 -translate-x-1/2
                 -translate-y-1/2
 
