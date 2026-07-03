8b3a5265d3b38a6ce3244a6a209742867b5f2b91
ridleylab <labridley@gmail.com>
2026-07-03 14:54:31 +0700

    bottom-0

--- src/components/sections/Beranda/Hero.tsx
+++ src/components/sections/Beranda/Hero.tsx
@@ -283,9 +283,9 @@ export default function Hero() {
 
                 absolute
                 bottom-0
-                left-0
+                left-1/2
                 -translate-x-1/2
-                -translate-y-1/2
+                translate-y-1/2
 
                 z-20
 
